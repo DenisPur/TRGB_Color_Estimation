a413a530afd7ebdcb9e77cd7fc8e1b5ad0df797e
Denis P. <d.g.purytin@gmail.com>
2024-09-19 22:09:39 +0300

    first page finished, second page created

---
+++ convert_phot_web/reader.py
@@ -0,0 +1,219 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+def define_type(file: Path | str) -> str:
+    """Rough approximation of a file type. Types correspond
+    to readers defines in edd_reader. 
+    """
+
+    with open(file) as f:
+        lines = f.readlines()
+        if lines[0][0:9] == 'extension':
+            data = pd.read_csv(file, delimiter='\s+', header=[0,], nrows=10)
+            if 'mag_mag1' in data.columns:
+                return 'type_1'
+            elif 'mag_V' in data.columns:
+                return 'type_2'
+        elif lines[1][0] == 'e':
+            data = pd.read_csv(file, delimiter='\s+', skiprows=[0,], nrows=10)
+            return 'type_3'
+        elif lines[1][0:3] == 'chp':
+            data = pd.read_csv(file, delimiter='\s+', skiprows=[0,], nrows=10)
+            return 'type_4'
+        else:
+            data = pd.read_csv(file, delimiter='\s+', header=None, nrows=10)
+            if len(data.columns) == 69:
+                return 'type_5'
+            else:
+                return 'type_6'
+
+
+##########################################################################
+
+
+def read_type1(file : str | Path) -> pd.DataFrame:
+    DICT_1 = {'mag_mag1' : 'mag_v', 'mag_mag2' : 'mag_i',
+          'snr_mag1' : 'snr_v', 'snr_mag2' : 'snr_i', 
+          'flag_mag1' : 'flag_v', 'flag_mag2' : 'flag_i', 
+          'crowd_mag1' : 'crowd_v', 'crowd_mag2' : 'crowd_i',
+          'sharp_mag1' : 'sharp_v', 'sharp_mag2' : 'sharp_i',
+          'mag1_err' : 'err_v', 'mag2_err' : 'err_i'}
+    data = pd.read_csv(file, header=0, delim_whitespace=True, index_col=False)
+    data = data[['extension', 'x', 'y',
+                 'mag_mag1', 'mag_mag2', 'type', 
+                 'snr_mag1', 'snr_mag2', 
+                 'flag_mag1', 'flag_mag2',
+                 'crowd_mag1', 'crowd_mag2',
+                 'sharp_mag1', 'sharp_mag2',
+                 'mag1_err', 'mag2_err']]
+    return data.rename(columns=DICT_1)
+
+
+def read_type2(file : str | Path) -> pd.DataFrame:
+    DICT_2 = {'mag_V' : 'mag_v', 'mag_I' : 'mag_i',
+          'snr_V' : 'snr_v', 'snr_I' : 'snr_i', 
+          'flag_V' : 'flag_v', 'flag_I' : 'flag_i',
+          'crowd_V' : 'crowd_v', 'crowd_I' : 'crowd_i',
+          'sharp_V' : 'sharp_v', 'sharp_I' : 'sharp_i',
+          'Verr' : 'err_v', 'Ierr' : 'err_i'}
+    data = pd.read_csv(file, header=0, delim_whitespace=True, index_col=False)
+    data = data[['extension', 'x', 'y',
+                 'mag_V', 'mag_I', 'type', 
+                 'snr_V', 'snr_I',
+                 'flag_V', 'flag_I',
+                 'crowd_V', 'crowd_I',
+                 'sharp_V', 'sharp_I',
+                 'Verr', 'Ierr']]
+    return data.rename(columns=DICT_2)
+
+
+def read_type3(file : str | Path) -> pd.DataFrame:
+    DICT_3 = {'e': 'extension', 'T' : 'type',
+          'X' : 'x', 'Y' : 'y',
+          'm_V' : 'mag_v', 'm_I' : 'mag_i', 
+          'SN.1' : 'snr_v', 'SN.2' : 'snr_i', 
+          'Eflag' : 'flag_v', 'Eflag.1' : 'flag_i',
+          'Crowd.1' : 'crowd_v', 'Crowd.2' : 'crowd_i',
+          'Sharp.1' : 'sharp_v', 'Sharp.2' : 'sharp_i',
+          'm_err' : 'err_v', 'm_err.1' : 'err_i'}
+    data = pd.read_csv(file, header=1, delim_whitespace=True, index_col=False)
+    data = data.rename(columns={'ex' : 'e', 'X' : 'x', 'Y' : 'y'})
+
+    if 'Crowd.2' in data.columns:
+        data = data[['e', 'x', 'y',
+                     'm_V', 'm_I', 'T', 
+                     'SN.1', 'SN.2',
+                     'Eflag', 'Eflag.1',
+                     'Crowd.1', 'Crowd.2',
+                     'Sharp.1', 'Sharp.2',
+                     'm_err', 'm_err.1']]
+        return data.rename(columns=DICT_3)
+
+    data = data[['e', 'x', 'y',
+                 'm_V', 'm_I', 'T', 
+                 'SN.1', 'SN.2',
+                 'Eflag', 'Eflag.1',
+                 'Sharp.1', 'Sharp.2',
+                 'm_err', 'm_err.1']]
+    return data.rename(columns=DICT_3)
+
+
+def read_type4(file : str | Path) -> pd.DataFrame:
+    DICT_4 = {'chp': 'extension', 'T' : 'type',
+        'X' : 'x', 'Y' : 'y',
+        'm_V' : 'mag_v', 'm_I' : 'mag_i', 
+        'SN.1' : 'snr_v', 'SN.2' : 'snr_i', 
+        'Eflag' : 'flag_v', 'Eflag.1' : 'flag_i',
+        'Crowd.1' : 'crowd_v', 'Crowd.2' : 'crowd_i',
+        'Sharp.1' : 'sharp_v', 'Sharp.2' : 'sharp_i',
+        'Round.1' : 'round_v', 'Round.2' : 'round_i',
+        'm_err' : 'err_v', 'm_err.1' : 'err_i'}
+    data = pd.read_csv(file, header=1, delim_whitespace=True, index_col=False)
+    columns = [col for col in list(DICT_4.keys()) if col in data.columns]
+    data = data[columns]
+    return data.rename(columns=DICT_4)
+
+
+def read_type5(file : str | Path) -> pd.DataFrame:
+    data = pd.read_csv(file, header=None, delim_whitespace=True, index_col=False)
+    data = data.iloc[:, [0, 1, 2, 8, 
+                         12, 13, 15, 16, 17, 18, 
+                         22, 23, 25, 26, 27, 28]]
+    data.columns = ['extension', 'x', 'y', 'type', 
+                    'mag_v', 'err_v', 'snr_v', 'sharp_v', 'round_v', 'crowd_v',
+                    'mag_i', 'err_i', 'snr_i', 'sharp_i', 'round_i', 'crowd_i']
+    return data
+
+
+def read_type6(file : str | Path) -> pd.DataFrame:
+    data = pd.read_csv(file, header=None, delim_whitespace=True, index_col=False)
+    data = data.iloc[:, [0, 2, 3, 10, 
+                         16, 17, 19, 20, 21, 22, 23, 
+                         29, 30, 32, 33, 34, 35, 36]]
+    data.columns = ['extension', 'x', 'y', 'type', 
+                    'mag_v', 'err_v', 'snr_v', 'sharp_v', 'round_v', 'crowd_v', 'flag_v',
+                    'mag_i', 'err_i', 'snr_i', 'sharp_i', 'round_i', 'crowd_i', 'flag_i']
+    return data
+
+
+##########################################################################
+
+
+def synch_coordinates_wfcs(phot):
+    x_array = phot['x'].to_numpy()
+    y_array = phot['y'].to_numpy()
+
+    x_rotated = x_array.copy()
+    y_rotated = y_array.copy()
+
+    kinda_size_of_a_chip = 750
+
+    for ind, ext in enumerate(phot['extension']):
+        if ext == 0:
+            x_rotated[ind] = kinda_size_of_a_chip + x_array[ind] * 0.5 - 75
+            y_rotated[ind] = kinda_size_of_a_chip + y_array[ind] * 0.5 - 75
+        elif ext == 1:
+            x_rotated[ind] = kinda_size_of_a_chip - y_array[ind]
+            y_rotated[ind] = kinda_size_of_a_chip + x_array[ind] - 100
+        elif ext == 2:
+            x_rotated[ind] = kinda_size_of_a_chip - x_array[ind]
+            y_rotated[ind] = kinda_size_of_a_chip - y_array[ind]
+        elif ext == 3:
+            x_rotated[ind] = kinda_size_of_a_chip + y_array[ind] - 100
+            y_rotated[ind] = kinda_size_of_a_chip - x_array[ind]
+
+    phot['x'] = x_rotated
+    phot['y'] = y_rotated
+
+    return phot
+
+
+def synch_coordinates_acs(phot):
+    phot['y'] = phot['y'] + 2100 * (phot['extension'] == 2)
+    return phot
+
+
+def file_has_broken_bites(filename : str | Path) -> bool:
+    with open(filename, newline='', encoding='utf-8') as f:
+        return '\0' in f.read()
+
+
+##########################################################################
+
+# another possible readers
+
+def read_type7(file : str | Path) -> pd.DataFrame:
+    data = pd.read_csv(file, header=None, delim_whitespace=True, index_col=False)
+    data = data.iloc[:, [0, 1, 2, 6, 
+                         10, 11, 13, 14, 
+                         18, 19, 21, 22]]
+    data.columns = ['extension', 'x', 'y', 'type', 
+                    'mag_v', 'err_v', 'snr_v', 'sharp_v',
+                    'mag_i', 'err_i', 'snr_i', 'sharp_i']
+    return data
+
+
+def read_type8(file : str | Path) -> pd.DataFrame:
+    data = pd.read_csv(file, header=None, delim_whitespace=True, index_col=False)
+    data = data.iloc[:, [0, 2, 3, 10, 
+                         14, 15, 17, 18, 19, 20, 21, 
+                         30, 31, 33, 34, 35, 36, 37]]
+    data.columns = ['extension', 'x', 'y', 'type', 
+                    'mag_v', 'err_v', 'snr_v', 'sharp_v', 'round_v', 'crowd_v', 'flag_v',
+                    'mag_i', 'err_i', 'snr_i', 'sharp_i', 'round_i', 'crowd_i', 'flag_i']
+    return data
+
+
+def read_type9(file : str | Path) -> pd.DataFrame:
+    DICT = {'Chp': 'extension', 'Type' : 'type',
+        'Xpos' : 'x', 'Ypos' : 'y',
+        'm_V' : 'mag_v', 'm_I' : 'mag_i', 
+        'S/N.1' : 'snr_v', 'S/N.2' : 'snr_i', 
+        'Eflag' : 'flag_v', 'Eflag.1' : 'flag_i',
+        'Crowd.1' : 'crowd_v', 'Crowd.2' : 'crowd_i',
+        'Sharp.1' : 'sharp_v', 'Sharp.2' : 'sharp_i',
+        'm_err' : 'err_v', 'm_err.1' : 'err_i'}
+    data = pd.read_csv(file, header=1, delim_whitespace=True, index_col=False)
+    return data.rename(columns=DICT)
---
+++ main.py
@@ -0,0 +1,246 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication, QWidget, QDialog, QFileDialog, QMessageBox
+)
+
+from main_window_ui import Ui_MainWindow
+from clearing_ui import Ui_Clearing_Dialog
+from masking_ui import Ui_Masking_Dialog
+from infra import (
+    read_file, simple_double_view, clearing_double_view, 
+    check_available_columns, masking_double_view, final_view
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.button_chose_file.clicked.connect(self.open_file)
+        self.ui.button_view_chosen.clicked.connect(self.first_preview)
+
+        self.ui.button_clearing.clicked.connect(self.open_clearing_dialog)
+        self.ui.button_view_clearing.clicked.connect(self.view_clearing_result)
+
+        self.ui.button_masking.clicked.connect(self.open_masking_dialog)
+        self.ui.button_view_masking.clicked.connect(self.view_masking_result)
+
+        self.ui.enter_mpcs.valueChanged.connect(self.mpcs_changed)
+        self.ui.enter_mags.valueChanged.connect(self.mags_changed)
+        self.ui.buttons_distance.accepted.connect(self.write_distance)
+        self.ui.buttons_distance.rejected.connect(self.empty_distance)
+
+        self.ui.enter_coef_1.valueChanged.connect(self.recalculate_reddening_labels)
+        self.ui.enter_coef_2.valueChanged.connect(self.recalculate_reddening_labels)
+        self.ui.enter_b_minus_v.valueChanged.connect(self.recalculate_reddening_labels)
+        self.ui.buttons_reddening.accepted.connect(self.write_reddening)
+        self.ui.buttons_reddening.rejected.connect(self.empty_reddening)
+
+        self.ui.button_final_view.clicked.connect(self.view_final)
+
+    def open_file(self):
+        widget = QWidget(parent=self)
+        self.file_path, _ = QFileDialog.getOpenFileName(widget, 'Open File')
+        if self.file_path:
+            print(self.file_path)
+            try:
+                self.data = read_file(self.file_path)
+                self.ui.label_filename.setText(self.file_path)
+                self.ui.button_view_chosen.setEnabled(True)
+                self.ui.group_clearing.setEnabled(True)
+
+            except pd.errors.ParserError:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText("Invalid file")
+                msg.setInformativeText('Chose .csv file with proper column names. Mandatory column names: ["x", "y", "mag_v", "err_v", "mag_i", "err_i"]')
+                msg.setWindowTitle("Error")
+                msg.exec_()
+
+    def first_preview(self):
+        simple_double_view(self.data)
+
+    def view_clearing_result(self):
+        self.clearing_res.show()
+
+    def view_masking_result(self):
+        self.masking_res.show()
+
+    def open_clearing_dialog(self):
+        window = QDialog(parent=self)
+        window.ui = Ui_Clearing_Dialog()
+        window.ui.setupUi(window)
+
+        columns_availability = check_available_columns(self.data)
+        window.ui.check_type.setChecked(columns_availability['type'])
+        window.ui.check_type.setEnabled(columns_availability['type'])
+        window.ui.check_mag.setChecked(columns_availability['mag'])
+        window.ui.check_mag.setEnabled(columns_availability['mag'])
+        window.ui.check_snr.setChecked(columns_availability['snr'])
+        window.ui.check_snr.setEnabled(columns_availability['snr'])
+        window.ui.check_sharp.setChecked(columns_availability['sharp'])
+        window.ui.check_sharp.setEnabled(columns_availability['sharp'])
+        window.ui.check_flag.setChecked(columns_availability['flag'])
+        window.ui.check_flag.setEnabled(columns_availability['flag'])
+        window.ui.check_crowd.setChecked(columns_availability['crowd'])
+        window.ui.check_crowd.setEnabled(columns_availability['crowd'])
+
+        def mark_clean_rows():
+            marking = (self.data['mag_v'] == self.data['mag_v'])
+
+            if window.ui.check_type.isChecked():
+                marking *= (self.data['type'] <= window.ui.enter_type.value())
+            if window.ui.check_mag.isChecked():
+                marking *= ((self.data['mag_v'] < window.ui.enter_mag.value()) & 
+                            (self.data['mag_i'] < window.ui.enter_mag.value()))
+            if window.ui.check_snr.isChecked():
+                marking *= ((self.data['snr_v'] >= window.ui.enter_snr.value()) & 
+                            (self.data['snr_i'] >= window.ui.enter_snr.value()))
+            if window.ui.check_sharp.isChecked():
+                marking *= ((self.data['sharp_v'] + self.data['sharp_i'])**2 <= window.ui.enter_sharp.value())
+            if window.ui.check_flag.isChecked():
+                marking *= ((self.data['flag_v'] <= window.ui.enter_flag.value()) & 
+                            (self.data['flag_i'] <= window.ui.enter_flag.value()))
+            if window.ui.check_crowd.isChecked():
+                marking *= ((self.data['crowd_v'] + self.data['crowd_i']) <= window.ui.enter_crowd.value())
+            return marking
+
+        def preview():
+            marking = mark_clean_rows()
+            clean_data = self.data[marking]
+            dirty_data = self.data[~marking]
+            fig = clearing_double_view(clean=clean_data, dirty=dirty_data)
+            fig.show()
+
+        def saving():
+            marking = mark_clean_rows()
+            clean_data = self.data[marking]
+            dirty_data = self.data[~marking]
+            self.clearing_res = clearing_double_view(clean=clean_data, dirty=dirty_data)
+            self.data = self.data[marking]
+            self.ui.button_view_clearing.setEnabled(True)
+            self.ui.group_masking.setEnabled(True)
+
+        window.ui.button_preview.clicked.connect(preview)
+        window.ui.buttonBox.accepted.connect(saving)
+        # window.ui.buttonBox.rejected.connect()
+        window.show()
+
+    def open_masking_dialog(self):
+        window = QDialog(parent=self)
+        window.ui = Ui_Masking_Dialog()
+        window.ui.setupUi(window)
+
+        x_min = self.data['x'].min()
+        x_max = self.data['x'].max()
+        y_min = self.data['y'].min()
+        y_max = self.data['y'].max()
+
+        window.ui.enter_x_left.setRange(x_min, x_max)
+        window.ui.enter_x_right.setRange(x_min, x_max)
+        window.ui.enter_x_left.setValue(x_min)
+        window.ui.enter_x_right.setValue(x_max)
+
+        window.ui.enter_y_bottom.setRange(y_min, y_max)
+        window.ui.enter_y_top.setRange(y_min, y_max)
+        window.ui.enter_y_bottom.setValue(y_min)
+        window.ui.enter_y_top.setValue(y_max)
+
+        def apply_mask():
+            x_left = window.ui.enter_x_left.value()
+            x_right = window.ui.enter_x_right.value()
+            x_left, x_right = sorted([x_left, x_right])
+            y_bottom = window.ui.enter_y_bottom.value()
+            y_top = window.ui.enter_y_top.value()
+            y_bottom, y_top = sorted([y_bottom, y_top])
+            borders = [x_left, x_right, y_bottom, y_top]
+
+            eps = 0.1
+            masking = ((self.data['x'] > x_left - eps) & 
+                       (self.data['x'] < x_right + eps) &
+                       (self.data['y'] > y_bottom - eps) &
+                       (self.data['y'] < y_top + eps))
+            return borders, masking
+
+        def preview():
+            borders, masking = apply_mask()
+            fig = masking_double_view(self.data, masking, borders)
+            fig.show()
+
+        def saving():
+            borders, masking = apply_mask()
+            self.masking_res = masking_double_view(self.data, masking, borders)
+            self.data = self.data[masking]
+            self.ui.button_view_masking.setEnabled(True)
+            self.ui.group_distance.setEnabled(True)
+
+        window.ui.button_preview.clicked.connect(preview)
+        window.ui.buttonBox.accepted.connect(saving)
+        # window.ui.buttonBox.rejected.connect()
+        window.show()
+
+    def mpcs_changed(self, value):
+        if (value > 0):
+            dist_mags = 5 * np.log10(value * 1e5)
+        else:
+            dist_mags = 0
+        self.ui.enter_mags.blockSignals(True)
+        self.ui.enter_mags.setValue(dist_mags)
+        self.ui.enter_mags.blockSignals(False)
+
+    def mags_changed(self, value):
+        dist_mpcs = 10**(value / 5) / 1e5
+        self.ui.enter_mpcs.blockSignals(True)
+        self.ui.enter_mpcs.setValue(dist_mpcs)
+        self.ui.enter_mpcs.blockSignals(False)
+
+    def write_distance(self):
+        self.dist = self.ui.enter_mags.value()
+        self.ui.group_reddening.setEnabled(True)
+
+    def empty_distance(self):
+        self.ui.enter_mags.setValue(0)
+        self.ui.enter_mpcs.setValue(0)
+
+    def write_reddening(self):
+        if (self.ui.tabs_reddening.currentIndex() == 0):
+            self.redshift = self.ui.enter_b_minus_v.value() * self.ui.enter_coef_1.value()
+            self.absorbtion = self.ui.enter_b_minus_v.value() * self.ui.enter_coef_2.value()
+        else:
+            self.redshift = self.ui.enter_redshift.value()
+            self.absorbtion = self.ui.enter_absorbtion.value()
+        self.ui.button_final_view.setEnabled(True)
+        self.ui.button_next_tab.setEnabled(True)
+
+    def empty_reddening(self):
+            self.ui.enter_b_minus_v.setValue(0.0)
+            self.ui.enter_coef_1.setValue(2.742)
+            self.ui.enter_coef_2.setValue(1.505)
+            self.ui.enter_redshift.setValue(0.0)
+            self.ui.enter_absorbtion.setValue(0.0)
+            self.ui.label_redshift.setText('na')
+            self.ui.label_absorbtion.setText('na')
+    
+    def recalculate_reddening_labels(self):
+        redshift = self.ui.enter_b_minus_v.value() * self.ui.enter_coef_1.value()
+        absorbtion = self.ui.enter_b_minus_v.value() * self.ui.enter_coef_2.value()
+        self.ui.label_redshift.setText(f'{redshift:1.4}')
+        self.ui.label_absorbtion.setText(f'{absorbtion:1.4}')
+    
+    def view_final(self):
+        fig = final_view(self.data, self.dist, self.redshift, self.absorbtion)
+        fig.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
